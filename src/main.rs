6d4e0211dd75548d4ec8dd1dc3044f5e80bc9bb6
W. Brian Quinn <w.brian.quinn@gmail.com>
2024-09-03 11:26:49 +0100

    feat: Use stderr for errors and info messages

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    eprint!("Searching for \"{}\" ", config.query);
+    eprintln!("in file \"{}\"", config.file_path);
 
     if let Err(e) = srep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
