7bb10dc19454966af05653dd359ae0c978b65d5f
W. Brian Quinn <w.brian.quinn@gmail.com>
2024-09-03 10:50:01 +0100

    feat: case-insensitive search internal

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,19 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     }
     results
 }
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<_> = Vec::new();
+
+    for line in contents.lines() {
+        //force both to lowercase
+        let line_lower = line.to_lowercase();
+        let query_lower = query.to_lowercase();
+        if line_lower.contains(&query_lower) {
+            results.push(line);
+        }
+    }
+    results
+}
 
 pub struct Config {
     pub query: String,
@@ -115,6 +128,32 @@ Pick three.";
             2,
             "should be 2 results but were {}",
             search(query, contents).len()
+        );
+    }
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rusT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
